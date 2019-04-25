28cc6d5ce26340a8b3116c449936fdb7cf8de4e4
gabriel <gabrielchaperonb@gmail.com>
2019-04-24 20:20:25 -0400

    weas del miercoles

--- evaluaciones/models.py
+++ evaluaciones/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Integrante(models.Model):
+    """Un turbo ejemplo"""
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Evaluador(models.Model):
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    # TODO: hacer bien esta wea, como tan flaite la contraseña
+    contraseña = models.CharField(max_length=2)
+
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+
+    class Meta:
+        verbose_name_plural = "Evaluadores"
+
+
+class Curso(models.Model):
+     nombre = models.CharField(max_length=200)
+     codigo = models.CharField(max_length=200)
+     sex_number = models.IntegerField()
+     año = models.IntegerField()
+     SEMESTER_CHOICES = (
+         ("1", "Otoño"),
+         ("2", "Primavera"),
+         ("3", "Verano"),
+     )
+     # TODO: como vamos a usar strings con numeros, que flaite
+     semester = models.CharField(max_length=1, choices=SEMESTER_CHOICES, default="2")
+
+     def __str__(self):
+        return self.nombre
--- iteracion1/settings.py
+++ iteracion1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'evaluaciones.apps.EvaluacionesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
