50262f6fbd69b42202db7e3a3fe97b5ef5c9bf47
Diego <diego.donoso@ing.uchile.cl>
2019-05-09 11:36:39 -0400

    Movimiento modelo curso

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=200)
+    sex_number = models.IntegerField()
+    año = models.IntegerField()
+    SEMESTER_CHOICES = (
+        ("1", "Otoño"),
+        ("2", "Primavera"),
+        ("3", "Verano"),
+    )
+    # TODO: como vamos a usar strings con numeros, que flaite
+    semester = models.CharField(max_length=1, choices=SEMESTER_CHOICES, default="2")
+
+    def __str__(self):
+        return self.nombre
--- evaluaciones/models.py
+++ evaluaciones/models.py
@@ -22,20 +22,3 @@ class Evaluador(models.Model):
 
     class Meta:
         verbose_name_plural = "Evaluadores"
-
-
-class Curso(models.Model):
-     nombre = models.CharField(max_length=200)
-     codigo = models.CharField(max_length=200)
-     sex_number = models.IntegerField()
-     año = models.IntegerField()
-     SEMESTER_CHOICES = (
-         ("1", "Otoño"),
-         ("2", "Primavera"),
-         ("3", "Verano"),
-     )
-     # TODO: como vamos a usar strings con numeros, que flaite
-     semester = models.CharField(max_length=1, choices=SEMESTER_CHOICES, default="2")
-
-     def __str__(self):
-        return self.nombre
