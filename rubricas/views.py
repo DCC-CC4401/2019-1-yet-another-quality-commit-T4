b2bdb203858509a98e911cd734e50dc7857a0d0a
luismontanaresm <luis.montanares.m@ug.uchile.cl>
2019-05-09 20:02:59 -0400

    Admin Rubrics Dynamic Content

--- rubricas/views.py
+++ rubricas/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def rubadmin(request):
-    return render(request, 'Admin_interface/Rubricas_admin.html')
+    return render(request, 'Admin_interface/Rubricas_admin.html',{
+        'rubs':['rubrica1','rubrica2','rubrica3']
+    }
+    )
 
 def fichaadmin(request):
     return render(request, 'FichasRubricas/FichaRubricaAdministrador.html')
