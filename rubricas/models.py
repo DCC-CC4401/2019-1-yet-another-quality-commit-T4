87316d0b02b1e799fc44f615fef778d58ac1806e
luismontanaresm <luis.montanares.m@ug.uchile.cl>
2019-05-15 20:54:01 -0400

    new model for rubric using files

--- rubricas/models.py
+++ rubricas/models.py
@@ -36,3 +36,33 @@ class Rubrica(models.Model):
     def getCont(self):
         return self.cont
 
+
+# Create your models here.
+class RubricaX(models.Model):
+    #Relation (nombre | codigoCurso | tarea | anno | semestre | aspecto | puntaje | contenido)
+    nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=200)
+    tarea = models.IntegerField()
+    anno = models.IntegerField()
+    SEMESTER_CHOICES = (
+        ("1", "Otoño"),
+        ("2", "Primavera"),
+        ("3", "Verano"),
+    )
+    semester = models.CharField(max_length=1, choices=SEMESTER_CHOICES, default="2")
+    path=nombre+codigo+tarea+anno+semester
+
+    def __str__(self):
+        return self.nombre
+    def getNombre(self):
+        return self.nombre
+    def getCodigo(self):
+        return self.codigo
+    def getTarea(self):
+        return self.tarea
+    def getAnno(self):
+        return self.anno
+    def getSemester(self):
+        return self.semester
+    def getPath(self):
+        return self.path
