92162efad4d625a742db3bd4a587d91b0d243cd6
gabriel <gabrielchaperonb@gmail.com>
2019-05-26 17:25:00 -0400

    agrega comando para agregar administradores

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # Aca deberian estar los modelos de los distintos usuarios que tengamos
+
+
+class Administrador(models.Model):
+    """Un turbo ejemplo"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # aca deberian ir mas campos, por ahora esta lo basico
+
+    def __str__(self):
+        return self.user.username
