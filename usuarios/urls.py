eb2343ef2921ae2f1f8a867c401f43d55e4db4f3
gabriel <gabrielchaperonb@gmail.com>
2019-06-02 19:22:10 -0400

    weasdokj

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('process/', views.process_login, name='process'),
     path('e/lp/', views.eval_loged, name='success_login_eval'),
-    path('e/eval/', views.evaluaciones_evaluador, name='evaluaciones_evaluador')
+    path('e/eval/', views.evaluaciones_evaluador, name='evaluaciones_evaluador'),
     path('a/lp/', views.landingpage, name='landing'), #Landing page de administrador
     path('a/ev/', views.evaluadores),  # Evaluadores visto por admin
     path('a/ev/delete/', views.delete_evaluador),
