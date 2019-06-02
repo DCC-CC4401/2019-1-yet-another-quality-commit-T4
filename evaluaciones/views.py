1e6abbf0fea577f9479f8bab1469741908ae6b2e
luismontanaresm <luis.montanares.m@ug.uchile.cl>
2019-06-01 21:37:38 -0400

    adjuntar rubrica a evaluacion

--- evaluaciones/views.py
+++ evaluaciones/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 import json
+from rubricas.processor import readCSV as reader
+from rubricas.models import Rubrica as R
 # Create your views here.
 def evaluacion(request):
-    ll = [["Aspecto","hola","hola"],["Aspecto2", "chao", "chao"]]
-    data = json.dumps(ll)
+    name="prueba"
+    r = R.objects.filter(nombre=name).first()
+    lista = reader(r)
+    lista.pop(0)
+    ll = [["Aspecto","hola","hola"],["Aspecto","hola","hola"],["Aspecto","hola","hola"],]
+    data = json.dumps(lista)
     return render(request, 'Eval_interface/evaluacion.html', context={'data':data})
 
 def evadmin(request):
