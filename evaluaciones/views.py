2bed1cea972e06f9830de85c0a221fc173fa51ad
luismontanaresm <luis.montanares.m@ug.uchile.cl>
2019-06-02 15:56:07 -0400

    hola

--- evaluaciones/views.py
+++ evaluaciones/views.py
@@ -6,11 +6,13 @@ from rubricas.models import Rubrica as R
 def evaluacion(request):
     name="prueba2"
     r = R.objects.filter(nombre=name).first()
-    lista = reader(r)
-    lista.pop(0)
+    data = reader(r)
+    header = reader(r)[0]
+    data.pop(0)
     ll = [["Aspecto","hola","hola"],["Aspecto","hola","hola"],["Aspecto","hola","hola"],]
-    data = json.dumps(lista)
-    return render(request, 'Eval_interface/evaluacion.html', context={'data':data})
+    data = json.dumps(data)
+    header = json.dumps(header)
+    return render(request, 'Eval_interface/evaluacion.html', context={'data':data,'header':header})
 
 def evadmin(request):
     return render(request, 'Eval_interface/ficha_evaluacion.html')
